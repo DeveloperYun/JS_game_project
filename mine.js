52bea13404429dca909e2d29096553b2f0f14668
DeveloperYun <yhb7588@naver.com>
2021-08-16 23:10:21 +0900

    지뢰찾기

--- mine.js
+++ mine.js
@@ -1,4 +1,11 @@
+var tbody = document.querySelector('#table tbody');
+var dataset = [];
+
 document.querySelector('#exec').addEventListener('click',function(){
+
+    tbody.innerHTML = ''; // tbody의 내부 태그들을 지운다.
+
+    // 문자열을 정수로 바꾸는 parse
     var hor = parseInt(document.querySelector('#hor').value);
     var ver = parseInt(document.querySelector('#ver').value);
     var mine = parseInt(document.querySelector('#mine').value);
@@ -24,10 +31,6 @@ document.querySelector('#exec').addEventListener('click',function(){
 
     console.log(shuffle);
 
-
-    var dataset = [];
-    var tbody = document.querySelector('#table tbody');
-
     // 입력 받은 hor와 ver 값에 따라 동적으로 tr, td 생성
     for(var i=0; i < ver; i+=1){
         var arr = [];
@@ -37,6 +40,70 @@ document.querySelector('#exec').addEventListener('click',function(){
         for(var j=0; j < hor; j+=1){
             arr.push(1);
             var td = document.createElement('td');
+
+            // 우클릭으로 깃발 꽂기
+            td.addEventListener('contextmenu',function(e){
+                e.preventDefault();
+
+                // 우클릭했을 때 좌표를 알 수 있게...(like 틱택토)
+                var parent_tr = e.currentTarget.parentNode;
+                var parent_tbody = e.currentTarget.parentNode.parentNode;
+                var 칸 = Array.prototype.indexOf.call(parent_tr.children, e.currentTarget);
+                var 줄 = Array.prototype.indexOf.call(parent_tbody.children, parent_tr);
+            
+                console.log(parent_tr, parent_tbody, e.currentTarget, 칸, 줄);
+
+                // 우클릭을 한번 더 하면 ? 를 표시한다
+                if(e.currentTarget.textContent === '' || e.currentTarget.textContent === 'X'){
+                    e.currentTarget.textContent='★';
+                   // dataset[줄][칸] = '★';
+                } 
+                else if(e.currentTarget.textContent === '★'){
+                    e.currentTarget.textContent='?';
+                   // dataset[줄][칸] = '?';
+                }
+                else{
+                    e.currentTarget.textContent='';
+                    //dataset[줄][칸] = '1'; // 데이터가 빈칸일 땐 1을 넣어줘야한다.
+
+                    if(dataset[줄][칸] === '1'){
+                        e.currentTarget.textContent ='';
+                    }
+                    else if(dataset[줄][칸] ==='X'){
+                        e.currentTarget.textContent = 'X';
+                    }
+                }
+            });
+
+            // click 했을 때 주변 지뢰 갯수 띄워주기
+            td.addEventListener('click',function(e){
+                var parent_tr = e.currentTarget.parentNode;
+                var parent_tbody = e.currentTarget.parentNode.parentNode;
+                var 칸 = Array.prototype.indexOf.call(parent_tr.children, e.currentTarget);
+                var 줄 = Array.prototype.indexOf.call(parent_tbody.children, parent_tr);
+
+                if(dataset[줄][칸] === 'X'){
+                    e.currentTarget.textContent = '펑';
+                }
+                else{
+                    var 주변 =[
+                        dataset[줄][칸-1],                    dataset[줄][칸+1]
+                    ];
+
+                    if(dataset[줄-1] != null){
+                        주변 = 주변.concat(dataset[줄-1][칸-1],dataset[줄-1][칸],dataset[줄-1][칸+1]);
+                    }
+                    if(dataset[줄+1] != null){
+                        주변 = 주변.concat(dataset[줄+1][칸-1],dataset[줄+1][칸],dataset[줄+1][칸+1]);
+                    }
+
+                    e.currentTarget.textContent = 주변.filter(function(v){
+                        return v === 'X';
+                    }).length;
+                }
+
+            });
+
             tr.appendChild(td);
         }
         tbody.appendChild(tr);
@@ -55,3 +122,9 @@ document.querySelector('#exec').addEventListener('click',function(){
    console.log(dataset);
 
 });
+
+// currentTarget 과 target의 차이를 보여주는 콘솔.
+tbody.addEventListener('contextmenu',function(e){
+    console.log('current target', e.currentTarget);
+    console.log('target',e.target);
+})
