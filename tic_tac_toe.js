6b5d26573c46a6676fbf1e200f605892a5db6a4a
DeveloperYun <yhb7588@naver.com>
2021-08-26 01:39:20 +0900

    틱택토_컴퓨터 구현

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -5,28 +5,8 @@ var cells = [];
 var turn = 'X'; // X가 선공
 var result = document.createElement('div');
 
-//익명으로 쓸 함수를 빼내서 간결성 높임
-//2차원 배열
-var clickFunc = function(e){
-    // console.log(e.target); // 이벤트 발생 요소 반환=>콘솔창 출력해줌
-    // console.log(e.target.parentNode); // 이벤트 발생 요소 부모 반환
-
-    //이벤트는 td에서 일어나지만 이벤트가 일어나는 "줄" 은 tr이므로 parentNode가 필요하다
-    var line_idx = lines.indexOf(e.target.parentNode); // 몇번쨰줄인지 반환
-    console.log('line_idx',line_idx);
-    var cols_idx = cells[line_idx].indexOf(e.target);
-    console.log('cols_idx',cols_idx);
- 
-    //칸이 채워져 있는지 확인
-    //value메소드는 input에서. 외에는 textContent
-    if(cells[line_idx][cols_idx].textContent !== ''){
-        console.log("its not blank");
-    } 
-    else{
-        console.log("its blank");
-        cells[line_idx][cols_idx].textContent = turn; // X로 표시하고
-
-        //틱택토가 완성됐는지(세칸이 다 채워졌는지) 확인하는 부분 (노가다)
+function 결과확인(line_idx,cols_idx){
+    //틱택토가 완성됐는지(세칸이 다 채워졌는지) 확인하는 부분 (노가다)
         // 1. 가로 빙고 검사
         var full = false;
         if(
@@ -43,43 +23,106 @@ var clickFunc = function(e){
                 full = true;
             }
         // 3. 대각선 빙고 검사
-        if(line_idx - cols_idx === 0 || Math.abs(line_idx-cols_idx) === 2){
+      //  if(line_idx - cols_idx === 0 || Math.abs(line_idx-cols_idx) === 2){
             if(
                 cells[0][0].textContent === turn &&
                 cells[1][1].textContent === turn &&
                 cells[2][2].textContent === turn){
                     full = true;
                 }
-            else if(
+            //else 
+            if(
                 cells[0][2].textContent === turn &&
                 cells[1][1].textContent === turn &&
                 cells[2][0].textContent === turn){
                     full = true;
                 }
-            }
-        
-            
-        // 4. 다 찼는지
-        if(full == true){
-            console.log(turn + ' is win');
-            result.textContent = turn + ' is win!';
+         //   }
+    return full;
+}
 
-            //승부 종료. 초기화
-            turn = 'X';
-            //forEach를 2번 써서 줄에서 칸으로 접근
-            cells.forEach(function(lines){
-                lines.forEach(function (cols){
-                    cols.textContent='';
-                });
+function 초기화(무승부) { // 초기화
+    if (무승부) {
+      result.textContent = '무승부';
+    } else { // 승부
+        result.textContent = turn + '님이 승리!';
+    }
+  
+    setTimeout(function() {
+      result.textContent = '';
+      cells.forEach(function (rows) {
+        rows.forEach(function (cols) {
+          cols.textContent = '';
+        });
+      });
+      turn = 'X';
+    }, 1000);
+  }
+
+
+//익명으로 쓸 함수를 빼내서 간결성 높임
+//2차원 배열
+var clickFunc = function(e){
+    // console.log(e.target); // 이벤트 발생 요소 반환=>콘솔창 출력해줌
+    // console.log(e.target.parentNode); // 이벤트 발생 요소 부모 반환
+
+    //이벤트는 td에서 일어나지만 이벤트가 일어나는 "줄" 은 tr이므로 parentNode가 필요하다
+    var line_idx = lines.indexOf(e.target.parentNode); // 몇번쨰줄인지 반환
+    //console.log('line_idx',line_idx);
+    var cols_idx = cells[line_idx].indexOf(e.target);
+    //console.log('cols_idx',cols_idx);
+ 
+    //칸이 채워져 있는지 확인
+    //value메소드는 input에서. 외에는 textContent
+    if(cells[line_idx][cols_idx].textContent !== ''){
+        console.log("its not blank");
+    } 
+    else{
+        console.log("its blank");
+        cells[line_idx][cols_idx].textContent = turn; // X로 표시하고
+
+        var 승리여부 = 결과확인(line_idx,cols_idx);
+
+        //빈 칸 중 하나를 고른다.
+        var 후보칸=[];
+        cells.forEach(function (rows){
+            rows.forEach(function (cols){
+                후보칸.push(cols);
             });
+        });
+        후보칸 = 후보칸.filter(function(cols){ //filter는 true인 것들을 거른다
+            return !cols.textContent; //빈칸만 남긴다
+        });
+
+        if(승리여부){
+            초기화();
+        }
+        else if(후보칸.length === 0){
+            초기화(true);
         }
         else{
-            if(turn === 'X'){
-                turn = 'O'; // O에게 턴을 넘긴다
+            if(turn==='X'){
+                turn = 'O';
             }
-            else{
+            // 컴퓨터의 턴
+            setTimeout(function(){
+                console.log('computers turn');
+
+                var 선택칸 = 후보칸[Math.floor(Math.random() * 후보칸.length)];
+                선택칸.textContent='O';
+                //computer가 승리한지 체크
+                var 몇줄 = lines.indexOf(선택칸.parentNode);
+                var 몇칸 = cells[몇줄].indexOf(선택칸);
+                var 승리여부 = 결과확인(몇줄,몇칸);
+                
+                //다 찼으면
+                if(승리여부){
+                    초기화();
+                }
+                //턴을 나에게 넘긴다
                 turn = 'X';
-            } 
+            },1000);
+
         }
     }
 }  
